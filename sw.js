b61add2bd5f5de82a4819cc50c34da0b2f5f2db0
ARJUN MANKAR PATIL <arjunmankararjun@gmail.com>
2025-08-15 00:36:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-const CACHE_NAME = 'freedom-runner-v1';
-const FILES_TO_CACHE = [
+const CACHE = 'freedom-runner-cache-v1';
+const FILES = [
   'index.html',
   'manifest.json',
-  'icons/icon-192.png',
-  'icons/icon-512.png'
+  'sw.js',
+  'icons/icon-192.svg',
+  'icons/icon-512.svg'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
   );
 });
